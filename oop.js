82d259096a31ecacc76ed2bee1893f77e6b8b5cf
Steve Rich <steven.john.rich@gmail.com>
2024-10-19 14:42:46 +0000

    Tweaked test names

--- oop.js
+++ oop.js
@@ -1,39 +1,31 @@
-PRIORITY = { "LOW": 1, "MEDIUM": 3, "HIGH": 5, "URGENT": 7 };
+const PRIORITY = { LOW: 1, MEDIUM: 3, HIGH: 5, URGENT: 7 };
 
-
-function validInteger (value) { // value can be a string or a number (integer)
-  
-}  
-
-
-function validatePriority(priority) { // value can be a string or a number (integer)
-  
+function validInteger(value) {
+  // value can be a string or a number (integer)
 }
 
-
-function todaysDate () {
-  
+function validatePriority(priority) {
+  // value can be a string or a number (integer)
 }
 
+function todaysDate() {
 
-class Task  {
+}
 
+class Task {
   // (title, priority)
 }
 
-
 class ToDo {
-    
+  
 }
-
-
-
-
-
-
-
 
 // Leave this code here for the automated tests
 module.exports = {
-  PRIORITY, validInteger, validatePriority, todaysDate, ToDo, Task,
-}
+  PRIORITY,
+  validInteger,
+  validatePriority,
+  todaysDate,
+  ToDo,
+  Task,
+};
